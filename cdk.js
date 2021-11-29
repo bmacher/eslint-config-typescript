7ba75ce990cd35cfd33078d730290019598797a7
Benjamin Macher <macher.benjamin@icloud.com>
2021-11-29 09:29:07 +0100

    feat: add cdk config

---
+++ cdk.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: [
+    './index.js',
+  ],
+  
+  rules: {
+    "no-new": "off",
+
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'objectLiteralProperty',
+        format: ['camelCase', 'UPPER_CASE', 'PascalCase']
+      },
+    ],
+  },
+};
+
